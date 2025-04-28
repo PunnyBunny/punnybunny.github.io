a67e4d38feaa67a8aeae2ef41936b18211b06f9d
Sunny Cheung <hyc55@cam.ac.uk>
2025-04-28 16:40:48 +0100

    minor

--- src/app/appbar.tsx
+++ src/app/appbar.tsx
@@ -5,15 +5,18 @@ const AppBar: React.FC = () => {
     <header className="w-full bg-black text-white p-4">
       <nav className="flex items-center justify-between">
         <div className="flex items-center gap-4">
-          <h1 className="text-xl font-bold">sunny</h1>
+          <h1 className="text-xl">
+            <span className="font-bold">sunny</span>
+            chenug
+          </h1>
         </div>
         <div className="flex items-center gap-4">
           <Link href="/" className="text-on-surface">
             about
           </Link>
-          <a href="/blog" className="text-on-surface">
+          <Link href="/blog" className="text-on-surface">
             blog
-          </a>
+          </Link>
         </div>
       </nav>
     </header>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const Page: React.FC = () => {
   return (
     <div className="p-10 flex flex-col gap-4">
       <div className="bg-black text-white p-4 w-fit">
-        <h1 className="text-7xl font-bold">hi. i am sunny.</h1>
+        <h1 className="text-7xl font-bold">hi, i&apos;m sunny.</h1>
       </div>
       <div className="flex flex-row">
         <div className="bg-black p-2 w-min" />
@@ -29,37 +29,22 @@ const Page: React.FC = () => {
       </div>
       <div className="grid grid-cols-3 gap-4">
         <div className="border-8 border-black p-2 h-min">
-          <Image src={fryegg} alt="Fried Egg" />
+          <Image src={fryegg} width={200} height={200} alt="Fried Egg" />
           <h1 className="text-2xl font-bold">project 1</h1>
           <p>
             some description <br /> some description
           </p>
         </div>
         <div className="border-8 border-black p-2 w-fit h-fit">
-          <Image
-            src="/images/fryegg.jpg"
-            alt="Fried Egg"
-            width={200}
-            height={200}
-          />
+          <Image src={fryegg} width={200} height={200} alt="Fried Egg" />
           <h1 className="text-2xl font-bold">project 2</h1>
         </div>
         <div className="border-8 border-black p-2 w-fit">
-          <Image
-            src="/images/fryegg.jpg"
-            alt="Fried Egg"
-            width={200}
-            height={200}
-          />
+          <Image src={fryegg} width={200} height={200} alt="Fried Egg" />
           <h1 className="text-2xl font-bold">project 3</h1>
         </div>
         <div className="border-8 border-black p-2 w-fit">
-          <Image
-            src="/images/fryegg.jpg"
-            alt="Fried Egg"
-            width={200}
-            height={200}
-          />
+          <Image src={fryegg} width={200} height={200} alt="Fried Egg" />
           <h1 className="text-2xl font-bold">project 4</h1>
         </div>
       </div>
