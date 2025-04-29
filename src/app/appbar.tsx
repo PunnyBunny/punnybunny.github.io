5c3d34d257a0539376f0c4b87f4b735c56832b25
Sunny Cheung <hyc55@cam.ac.uk>
2025-04-29 16:24:16 +0100

    fix scrolling

--- src/app/appbar.tsx
+++ src/app/appbar.tsx
@@ -4,25 +4,60 @@ import React from "react";
 import Link from "next/link";
 import { Bars3Icon } from "@heroicons/react/16/solid";
 
-const navigationItems = [
-  {
-    label: "about",
-    href: "/",
-  },
-  {
-    label: "blog",
-    href: "/blog",
-  },
-];
-
 const AppBar: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
+  const openMenu = () => {
+    setIsMenuOpen(true);
+    document.body.classList.add("overflow-hidden");
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+    document.body.classList.remove("overflow-hidden");
+  };
+
+  const toggleMenu = () => {
+    if (isMenuOpen) {
+      closeMenu();
+    } else {
+      openMenu();
+    }
+  };
+
+  const links = (
+    <>
+      <button
+        onClick={() => {
+          document.documentElement.classList.toggle("dark");
+          closeMenu();
+        }}
+        className="bg-surface dark:bg-dark-surface text-on-surface dark:text-on-dark-surface hover:bg-gray-800 transition duration-300"
+      >
+        toggle theme
+      </button>
+      <Link
+        href="/"
+        onClick={closeMenu}
+        className="text-on-surface dark:text-on-dark-surface"
+      >
+        about
+      </Link>
+      <Link
+        href="/blog"
+        onClick={closeMenu}
+        className="text-on-surface dark:text-on-dark-surface"
+      >
+        blog
+      </Link>
+    </>
+  );
+
   return (
-    <header className="w-full bg-surface dark:bg-dark-surface text-on-surface dark:text-on-dark-surface p-4">
-      <nav className="flex items-center justify-between">
+    <header className="w-full bg-surface dark:bg-dark-surface text-on-surface dark:text-on-dark-surface">
+      <nav>
         <div className="flex flex-col w-full h-full">
-          <div className="flex flex-row items-center justify-between w-full">
+          <div className="flex flex-row items-center justify-between w-full h-16 p-4">
             <Link href="/">
               <div className="flex items-center gap-4">
                 <h1 className="text-xl">
@@ -31,46 +66,16 @@ const AppBar: React.FC = () => {
                 </h1>
               </div>
             </Link>
-            <div className="md:hidden">
-              <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <div className="md:hidden flex items-center justify-center">
+              <button onClick={toggleMenu}>
                 <Bars3Icon className="h-6 w-6 text-on-surface dark:text-on-dark-surface" />
               </button>
             </div>
-            <div className="hidden md:flex items-center gap-4">
-              <button
-                onClick={() =>
-                  document.documentElement.classList.toggle("dark")
-                }
-                className="bg-surface dark:bg-dark-surface text-on-surface dark:text-on-dark-surface p-2 rounded-md hover:bg-gray-800 transition duration-300"
-              >
-                toggle theme
-              </button>
-              <Link
-                href="/"
-                className="text-on-surface dark:text-on-dark-surface"
-              >
-                about
-              </Link>
-              <Link
-                href="/blog"
-                className="text-on-surface dark:text-on-dark-surface"
-              >
-                blog
-              </Link>
-            </div>
+            <div className="hidden md:flex items-center gap-4">{links}</div>
           </div>
           {isMenuOpen && (
-            <div className="top-0 left-0 w-full h-screen bg-surface dark:bg-dark-surface p-4">
-              {navigationItems.map((item) => (
-                <Link
-                  key={item.label}
-                  href={item.href}
-                  className="block text-on-surface dark:text-on-dark-surface py-2"
-                  onClick={() => setIsMenuOpen(false)}
-                >
-                  {item.label}
-                </Link>
-              ))}
+            <div className="fixed top-16 left-0 bottom-0 w-full bg-surface dark:bg-dark-surface p-4 z-10 flex flex-col gap-4 items-center justify-center">
+              {links}
             </div>
           )}
         </div>
