abf02083183c9b7ad22f3e87583decb0806deaa9
Sunny Cheung <hyc55@cam.ac.uk>
2025-05-03 01:01:50 +0100

    add more blogs

---
+++ public/content/blogs/dinner-02feb25.md
@@ -0,0 +1,9 @@
+---
+title: "Dinner"
+date: "2025-05-02"
+description: "salty"
+---
+
+![dinner](/content/blogs/images/dinner-02feb25.png)
+
+A little bit too salty, as Raphael pours half of his fish sauce into the green curry. Smells really good nevertheless, but can be better. Also he said he'll bring bell pepper and then he brought other stuff and forgot the bell pepper at last.
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,24 +4,28 @@ import Link from "next/link";
 const Blogs: React.FC = () => {
   return (
     <div className="p-10 flex flex-col gap-4 min-h-screen">
-      <div className="bg-surface dark:bg-dark-surface text-on-surface dark:text-on-dark-surface p-4 w-fit">
+      <div className="bg-surface dark:bg-dark-surface text-on-surface dark:text-on-dark-surface p-4 w-fit self-center">
         <h1 className="text-7xl font-bold">blogs</h1>
       </div>
-      {getBlogSlugs().map((slug) => {
-        const blog = getBlogBySlug(slug);
-        return (
-          <div key={slug} className="flex flex-row">
-            <div className="bg-surface dark:bg-dark-surface p-2 w-min" />
-            <div className="flex flex-col p-4">
-              <Link href={`/blog/${slug}`}>
-                <h1 className="text-2xl font-bold">{blog.title}</h1>
-              </Link>
-              <p>{blog.description}</p>
-              <p>{blog.date}</p>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {getBlogSlugs()
+          .map((slug) => getBlogBySlug(slug))
+          .sort(
+            (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
+          )
+          .map((blog) => (
+            <div key={blog.slug} className="flex flex-row">
+              <div className="bg-surface dark:bg-dark-surface p-2 w-min" />
+              <div className="flex flex-col p-4">
+                <Link href={`/blog/${blog.slug}`}>
+                  <h1 className="text-2xl font-bold">{blog.title}</h1>
+                </Link>
+                <p>{blog.description}</p>
+                <p>{blog.date}</p>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          ))}
+      </div>
     </div>
   );
 };
